0b11f39383be7383ddac5b196c61b79b8e0b4ed9
LachlanBWWright <lachlanbwwright@gmail.com>
2025-01-02 02:10:07 +1100

    Fix Konva container sizing

--- frontend/src/editor/EditorView.tsx
+++ frontend/src/editor/EditorView.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Button, ZoomButton } from "../components/Button";
 import { ottoMaticLevel } from "../python/structSpecs/ottoMaticInterface";
 import { Fences } from "./subviews/Fences";
@@ -64,6 +64,8 @@ export function EditorView({
       stage.scale = Math.min(5, stage.scale * 0.9);
     });
 
+  const stageRef = useRef<HTMLDivElement>(null);
+
   return (
     <div className="flex flex-col flex-1 w-full gap-2 min-h-0">
       <div className="grid grid-cols-7 gap-2 w-full overflow-clip">
@@ -123,124 +125,124 @@ export function EditorView({
         )}
         {view === View.topology && <TopologyMenu />}
       </div>
-      <Stage
-        width={2000}
-        height={2000}
-        scaleX={stage.scale}
-        scaleY={stage.scale}
-        x={stage.x}
-        y={stage.y}
-        draggable={true}
-        onClick={(e) => {
-          console.log("TEST");
-          if (clickToAddItem === undefined) return;
-          const stage = e.target.getStage();
-
-          const pos = stage?.getRelativePointerPosition();
-          if (!pos) return;
-          const x = Math.round(pos.x);
-          const z = Math.round(pos.y);
-          console.log("SETDATA", x, z);
-          console.log(data.Itms[1000].obj);
-          console.log(data.Itms[1000].obj.length);
-
-          setData((data) => {
-            data.Itms[1000].obj.push({
-              x: x,
-              z: z,
-              type: clickToAddItem,
-              flags: 0,
-              p0: 0,
-              p1: 0,
-              p2: 0,
-              p3: 0,
-            });
-          });
-        }}
-        onDblClick={() => {
-          setSelectedFence(undefined);
-          setSelectedItem(undefined);
-          setSelectedSpline(undefined);
-          setSelectedWaterBody(undefined);
-        }}
-        onWheel={(e) => {
-          /*from https://stackoverflow.com/questions/52054848/how-to-react-konva-zooming-on-scroll */
-          e.evt.preventDefault();
-
-          const scaleBy = 1.05;
-          const stage = e.target.getStage();
-          if (!stage) return;
-          const oldScale = stage.scaleX();
-          const pointerPosition = stage.getPointerPosition();
-          if (!pointerPosition) return;
-
-          const mousePointTo = {
-            x: pointerPosition.x / oldScale - stage.x() / oldScale,
-            y: pointerPosition.y / oldScale - stage.y() / oldScale,
-          };
-
-          const newScale =
-            e.evt.deltaY < 0 ? oldScale * scaleBy : oldScale / scaleBy;
-
-          setStage({
-            scale: newScale,
-            x: (pointerPosition.x / newScale - mousePointTo.x) * newScale,
-            y: (pointerPosition.y / newScale - mousePointTo.y) * newScale,
-          });
-        }}
+      <div
         className="w-full min-h-0 flex-1 border-2 border-black overflow-clip"
+        ref={stageRef}
       >
-        {view === View.topology ? (
-          <TopologyGrid
-            data={data}
-            setData={setData}
-            isEditingTopology={view === View.topology}
-          />
-        ) : (
-          <Tiles data={data} mapImages={mapImages} />
-        )}
-        {view === View.topology ||
-          (view === View.tiles && (
+        <Stage
+          width={stageRef.current?.offsetWidth ?? 3000}
+          height={stageRef.current?.offsetHeight ?? 2000}
+          scaleX={stage.scale}
+          scaleY={stage.scale}
+          x={stage.x}
+          y={stage.y}
+          draggable={true}
+          onClick={(e) => {
+            if (clickToAddItem === undefined) return;
+            const stage = e.target.getStage();
+
+            const pos = stage?.getRelativePointerPosition();
+            if (!pos) return;
+            const x = Math.round(pos.x);
+            const z = Math.round(pos.y);
+
+            setData((data) => {
+              data.Itms[1000].obj.push({
+                x: x,
+                z: z,
+                type: clickToAddItem,
+                flags: 0,
+                p0: 0,
+                p1: 0,
+                p2: 0,
+                p3: 0,
+              });
+            });
+          }}
+          onDblClick={() => {
+            setSelectedFence(undefined);
+            setSelectedItem(undefined);
+            setSelectedSpline(undefined);
+            setSelectedWaterBody(undefined);
+          }}
+          onWheel={(e) => {
+            /*from https://stackoverflow.com/questions/52054848/how-to-react-konva-zooming-on-scroll */
+            e.evt.preventDefault();
+
+            const scaleBy = 1.05;
+            const stage = e.target.getStage();
+            if (!stage) return;
+            const oldScale = stage.scaleX();
+            const pointerPosition = stage.getPointerPosition();
+            if (!pointerPosition) return;
+
+            const mousePointTo = {
+              x: pointerPosition.x / oldScale - stage.x() / oldScale,
+              y: pointerPosition.y / oldScale - stage.y() / oldScale,
+            };
+
+            const newScale =
+              e.evt.deltaY < 0 ? oldScale * scaleBy : oldScale / scaleBy;
+
+            setStage({
+              scale: newScale,
+              x: (pointerPosition.x / newScale - mousePointTo.x) * newScale,
+              y: (pointerPosition.y / newScale - mousePointTo.y) * newScale,
+            });
+          }}
+        >
+          {view === View.topology ? (
+            <TopologyGrid
+              data={data}
+              setData={setData}
+              isEditingTopology={view === View.topology}
+            />
+          ) : (
+            <Tiles data={data} mapImages={mapImages} />
+          )}
+          {view === View.topology ||
+            (view === View.tiles && (
+              <>
+                <WaterBodies data={data} setData={setData} />
+                <Fences data={data} setData={setData} />
+                <Items data={data} setData={setData} />
+                <Splines data={data} setData={setData} />
+              </>
+            ))}
+          {view === View.fences && (
             <>
               <WaterBodies data={data} setData={setData} />
+              <Items data={data} setData={setData} />
+              <Splines data={data} setData={setData} />
+              <Fences data={data} setData={setData} />
+            </>
+          )}
+          {view === View.water && (
+            <>
               <Fences data={data} setData={setData} />
               <Items data={data} setData={setData} />
               <Splines data={data} setData={setData} />
+              <WaterBodies data={data} setData={setData} />
             </>
-          ))}
-        {view === View.fences && (
-          <>
-            <WaterBodies data={data} setData={setData} />
-            <Items data={data} setData={setData} />
-            <Splines data={data} setData={setData} />
-            <Fences data={data} setData={setData} />
-          </>
-        )}
-        {view === View.water && (
-          <>
-            <Fences data={data} setData={setData} />
-            <Items data={data} setData={setData} />
-            <Splines data={data} setData={setData} />
-            <WaterBodies data={data} setData={setData} />
-          </>
-        )}
-        {view === View.splines && (
-          <>
-            <WaterBodies data={data} setData={setData} />
-            <Items data={data} setData={setData} />
-            <Fences data={data} setData={setData} />
-            <Splines data={data} setData={setData} />
-          </>
-        )}
-        {view === View.items && (
-          <>
-            <WaterBodies data={data} setData={setData} />
-            <Splines data={data} setData={setData} />
-            <Fences data={data} setData={setData} />
-            <Items data={data} setData={setData} />
-          </>
-        )}
-      </Stage>
+          )}
+          {view === View.splines && (
+            <>
+              <WaterBodies data={data} setData={setData} />
+              <Items data={data} setData={setData} />
+              <Fences data={data} setData={setData} />
+              <Splines data={data} setData={setData} />
+            </>
+          )}
+          {view === View.items && (
+            <>
+              <WaterBodies data={data} setData={setData} />
+              <Splines data={data} setData={setData} />
+              <Fences data={data} setData={setData} />
+              <Items data={data} setData={setData} />
+            </>
+          )}
+        </Stage>
+      </div>
     </div>
   );
 }
