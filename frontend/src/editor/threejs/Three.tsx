486d800be5b6bfa843bca7d9edee91b35b51ffde
LachlanBWWright <lachlanbwwright@gmail.com>
2025-05-10 18:11:26 +1000

    Update default camera position

--- frontend/src/editor/threejs/Three.tsx
+++ frontend/src/editor/threejs/Three.tsx
@@ -15,7 +15,13 @@ export function ThreeView({
   mapImages: HTMLCanvasElement[];
 }) {
   const globals = useAtomValue(Globals);
-  const header = data.Hedr?.[1000]?.obj;
+  const header = data.Hedr[1000].obj;
+
+  const numWide = header.mapWidth;
+  const numHigh = header.mapHeight;
+
+  const unitsWide = numWide * globals.TILE_INGAME_SIZE;
+  const unitsHigh = numHigh * globals.TILE_INGAME_SIZE;
 
   // Ensure header is defined before rendering TestGeometry
   if (!header) {
@@ -24,7 +30,13 @@ export function ThreeView({
 
   return (
     <Canvas
-      camera={{ fov: 110, near: 0.1, far: 100000, position: [0, 0, 100] }}
+      camera={{
+        fov: 110,
+        near: 0.1,
+        far: 100000,
+        position: [unitsWide / 2, 5000, unitsHigh / 2],
+        rotation: [0, Math.PI, 0],
+      }}
     >
       <TrackballControls />
       <TerrainGeometry data={data} mapImages={mapImages} />
