5ba1d2255d7d2451ee3d5b381fe1b5a057cb89d3
LachlanBWWright <lachlanbwwright@gmail.com>
2025-12-31 18:44:11 +1100

    Update linter

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,12 +5,27 @@ import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 import reactHooks from "eslint-plugin-react-hooks";
 
-export default defineConfig([
-  eslint.configs.recommended,
-  tseslint.configs.strict,
-  tseslint.configs.stylistic,
-  reactHooks.configs.flat.recommended,
-]);
+export default defineConfig(
+  [
+    eslint.configs.recommended,
+    tseslint.configs.strict,
+    tseslint.configs.stylistic,
+    reactHooks.configs.flat.recommended,
+  ],
+  {
+    plugins: {
+      "typescript-eslint": tseslint,
+    },
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unsafe-type-assertion": "error",
+    },
+  },
+);
 
 /* import js from "@eslint/js";
 import globals from "globals";
